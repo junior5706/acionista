4c3511c3990a9150d8819d8d5fc0dee4e6b1fff7
Junior Silva <junior5706@outlook.com>
2024-01-20 14:12:51 -0300

    ajuste final

--- acionista.py
+++ acionista.py
@@ -14,7 +14,7 @@ minPVP = 0.5
 maxPVP = 2
 minPL = 3
 maxPL = 10
-minDivYield = 0.06  # 6%
+minDivYield = 0.07  # 6%
 maxDivYield = 0.14  # 14%
 minROE = 0.15  # 15%
 maxROE = 0.30  # 30%
@@ -43,24 +43,20 @@ df_filtrado = df[
     (df['Cresc. Rec.5a'] > crescimento_minimo)
 ]
 
-# Ordenar por PL do menor para o maior
-df_filtrado.sort_values(by='P/L', inplace=True)
-# Ordenar por PVP do menor para o maior
-df_filtrado.sort_values(by='P/VP', inplace=True)
-# Ordenar por Dividend Yield do maior para o menor
-df_filtrado.sort_values(by='Div.Yield', inplace=True, ascending=False)
-# Ordenar por ROE do maior para o menor
-df_filtrado.sort_values(by='ROE', inplace=True, ascending=False)
-# Ordenar por Dív.Brut/ Patrim. do menor para o maior
-df_filtrado.sort_values(by='Dív.Brut/ Patrim.', inplace=True)
-
 # Criar um campo de pontuação no data frame para ranquear as ações de acordo com os critérios de ordenação acima considerando o top 5 de cada ordenação para somar 1 ponto a cada ação que aparecer no top 5
+# Inicializa a pontuação
 df_filtrado['Pontuação'] = 0
-df_filtrado.loc[df_filtrado['P/L'] <= df_filtrado['P/L'].quantile(0.2), 'Pontuação'] += 1
-df_filtrado.loc[df_filtrado['P/VP'] <= df_filtrado['P/VP'].quantile(0.2), 'Pontuação'] += 1
-df_filtrado.loc[df_filtrado['Div.Yield'] >= df_filtrado['Div.Yield'].quantile(0.8), 'Pontuação'] += 1
-df_filtrado.loc[df_filtrado['ROE'] >= df_filtrado['ROE'].quantile(0.8), 'Pontuação'] += 1
-df_filtrado.loc[df_filtrado['Dív.Brut/ Patrim.'] <= df_filtrado['Dív.Brut/ Patrim.'].quantile(0.2), 'Pontuação'] += 1
+# Adiciona 1 ponto para cada ação que aparecer no top 5 de cada ordenação por P/L, P/VP, Div.Yield, ROE e Dív.Brut/ Patrim.
+top_pl = df_filtrado.nsmallest(5, 'P/L')['papel']
+df_filtrado.loc[df_filtrado['papel'].isin(top_pl), 'Pontuação'] += 1
+top_pvp = df_filtrado.nsmallest(5, 'P/VP')['papel']
+df_filtrado.loc[df_filtrado['papel'].isin(top_pvp), 'Pontuação'] += 1
+top_divyield = df_filtrado.nlargest(5, 'Div.Yield')['papel']
+df_filtrado.loc[df_filtrado['papel'].isin(top_divyield), 'Pontuação'] += 1
+top_roe = df_filtrado.nlargest(5, 'ROE')['papel']
+df_filtrado.loc[df_filtrado['papel'].isin(top_roe), 'Pontuação'] += 1
+top_divbrutpatrim = df_filtrado.nsmallest(5, 'Dív.Brut/ Patrim.')['papel']
+df_filtrado.loc[df_filtrado['papel'].isin(top_divbrutpatrim), 'Pontuação'] += 1
 
 # Ordenar por Pontuação do maior para o menor
 df_filtrado.sort_values(by='Pontuação', inplace=True, ascending=False)
@@ -74,20 +70,13 @@ df_filtrado['ROE'] = df_filtrado['ROE'] * 100
 # Converter a coluna 'Cresc. Rec.5a' para porcentagem legível
 df_filtrado['Cresc. Rec.5a'] = df_filtrado['Cresc. Rec.5a'] * 100
 
-# Converter a coluna 'PSR' para porcentagem legível
-df_filtrado['PSR'] = df_filtrado['PSR'] * 100
-
-# Converter a coluna 'P/Ativo' para porcentagem legível
-df_filtrado['P/Ativo'] = df_filtrado['P/Ativo'] * 100
-
-# Converter a coluna 'Mrg Ebit' para porcentagem legível
-df_filtrado['Mrg Ebit'] = df_filtrado['Mrg Ebit'] * 100
-
-# Converter a coluna 'Mrg. Líq.' para porcentagem legível
-df_filtrado['Mrg. Líq.'] = df_filtrado['Mrg. Líq.'] * 100
-
-# Converter a coluna 'ROIC' para porcentagem legível
-df_filtrado['ROIC'] = df_filtrado['ROIC'] * 100
+# Excluir colunas que não serão utilizadas no data frame final
+colunas_para_excluir = [
+    'PSR', 'P/Ativo', 'P/Cap.Giro', 'P/EBIT', 'P/Ativ Circ.Liq',
+    'EV/EBIT', 'EV/EBITDA', 'Mrg Ebit', 'Mrg. Líq.',
+    'Liq. Corr.', 'ROIC'
+]
+df_filtrado.drop(colunas_para_excluir, axis=1, inplace=True)
 
 # Verificar se o arquivo resultado.xlsx existe no path atual e se existir apagar
 if os.path.exists('resultado.xlsx'):
